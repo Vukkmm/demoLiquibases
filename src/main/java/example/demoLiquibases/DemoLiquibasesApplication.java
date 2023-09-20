c90119ff9f0b2d60171a8d091ada0560bfb743a4
Nguyen Van Vu <vu27082002@gamil.com>
2023-09-20 21:39:32 +0700

    Thêm dự án demoLiquibases lên Git

---
+++ src/main/java/example/demoLiquibases/DemoLiquibasesApplication.java
@@ -0,0 +1,13 @@
+package example.demoLiquibases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoLiquibasesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoLiquibasesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/example/demoLiquibases/DemoLiquibasesApplicationTests.java
@@ -0,0 +1,13 @@
+package example.demoLiquibases;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoLiquibasesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
